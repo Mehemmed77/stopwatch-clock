9922454b894c1bf7e33728891bfb3d0b736c7b48
Mehemmed77 <mehemmedkazimzade54@gmail.com>
2023-09-01 02:29:01 +0400

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [time,setTime] = useState(0);
+	const [timeon,setTimeon] = useState(false);
+
+	useEffect(() => {
+		let interval = null;
+		
+		if(timeon){
+			interval = setInterval(() => {
+				setTime(time => time + 1);
+			},1000);
+
+		} else{
+			clearInterval(interval);
+		};
+
+		return () => clearInterval(interval)
+
+	},[timeon]);
+
+	const resetTime = () => {
+		setTime(0)
+		setTimeon(false);
+	}
+
+	return (
+		<div className="container">
+			<div className="timer">
+				<p id="time">
+					<span id="hours">{("0" + parseInt(time / 3600)).slice(-2)}</span>:
+					<span id="mins">{("0" + parseInt(time / 60)).slice(-2)}</span>:
+					<span id="secs">{("0" + time % 60).slice(-2)}</span>
+				</p>
+			</div>
+			<div className="button-container">
+				<button className="btn" id="start" onClick={() => setTimeon(true)}>Start</button>
+				<button className="btn" id="stop" onClick={() => setTimeon(false)}>Stop</button>
+				<button className="btn" id="reset" onClick={resetTime}>Reset</button>
+			</div> 
+		</div>
+	);
 }
 
 export default App;
